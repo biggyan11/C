74f6751bb0386c6004b9749ff52f98c6e35395c0
Biggyan Aryal <biggyanaryal07@gmail.com>
2025-11-20 19:47:56 +0545

    Add files via upload

---
+++ Untitled7.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+
+int main() {
+    float l, area;
+
+    printf("Enter side length: ");
+    scanf("%f", &l);
+
+    area = l* l;
+
+    printf("Area = %f\n", area);
+    return 0;
+}

97af3e56169a7a799addc26536cb34f1cef3c931
Biggyan Aryal <biggyanaryal07@gmail.com>
2025-11-20 19:32:49 +0545

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float p, t, r, si;
+
+    printf("Enter Principal, Time, Rate: ");
+    scanf("%f %f %f", &p, &t, &r);
+
+    si = (p * t * r) / 100;
+
+    printf("Simple Interest = %.2f\n", si);
+    return 0;
+}
